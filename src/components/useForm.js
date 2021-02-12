769051573e0259deba64dfa98042ad971edd5ae3
JonathanMary <jon85ma@gmail.com>
2021-02-12 14:50:30 +0100

    MVP: Checkout Form Done

---
+++ src/components/useForm.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export const useForm =  (initialValue) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+    };
+  
+
+    return [showSuccessMessage, values, handleChanges, handleSubmit]
+}
